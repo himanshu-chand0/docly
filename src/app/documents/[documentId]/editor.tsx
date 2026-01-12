ff3d923a569090f7f7b3ede37ca3ae36dd4a8c1b
Himanshu Chand <himanshu.chand@icloud.com>
2026-01-12 13:03:40 +0545

    commit

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -23,10 +23,13 @@ import TextAlign from '@tiptap/extension-text-align'
 import { useEditorStore } from '@/lib/use-editor-store';
 import { FontSizeExtensions } from "@/extensions/font-size";
 import { LineHeightExtension } from '@/extensions/line-height';
+import { Ruler } from './ruler';
 
 export const Editor = () => {
     const {setEditor} = useEditorStore();
+    
     const editor = useEditor({
+         immediatelyRender: false,
         onCreate({editor}) {
             setEditor(editor);
         },
@@ -60,7 +63,10 @@ export const Editor = () => {
         extensions: [StarterKit,
             Image,
             Color,
-            LineHeightExtension,
+            LineHeightExtension.configure({
+                types: ['paragraph', 'heading'],
+                defaultLineHeight: 'normal',
+            }),
             FontSizeExtensions,
             Highlight.configure({
                 multicolor: true,
@@ -103,6 +109,7 @@ export const Editor = () => {
     })
     return (
         <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
+            <Ruler />
             <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
                 <EditorContent editor={editor} />
             </div>

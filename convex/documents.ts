f46f14268c9e2997d87ef677ce504e5b7d3a0f0b
John Doe <johndoe@example.com>
2026-01-13 04:24:05 +0545

    ttt

--- convex/documents.ts
+++ convex/documents.ts
@@ -12,7 +12,9 @@ export const create = mutation({
       throw new ConvexError("Unathorized");
     }
 
-    const organizationId = (user.organization_id ?? undefined) as string | undefined;
+    const organizationId = (user.organization_id ?? undefined) as
+    | string 
+    | undefined;
 
     return ctx.db.insert("documents", {
       title: args.title ?? "Untitled document",
@@ -25,8 +27,41 @@ export const create = mutation({
 
 export const get = query({
     args: { paginationOpts: paginationOptsValidator, search: v.optional(v.string()) },
-    handler: async (ctx, args) => {
-        return await ctx.db.query("documents").paginate(args.paginationOpts);
+    handler: async (ctx, {search,paginationOpts}) => {
+      const user=await ctx.auth.getUserIdentity();
+
+      if (!user) {
+        throw new ConvexError("Unauthorized");
+      }
+
+      const organizationId = (user.organization_id ?? undefined) as
+      | string
+      | undefined
+
+      if (search && organizationId) {
+        return await ctx.db
+        .query("documents")
+        .withSearchIndex("search_title", (q) =>
+          q.search("title", search).eq("organizationId", organizationId)
+        )
+        .paginate(paginationOpts)
+      }
+
+      if (search) {
+        return await ctx.db
+        .query("documents")
+        .withSearchIndex("search_title", (q) => q.search("title", search).eq("ownerId", user.subject)
+        )
+        .paginate(paginationOpts)   
+      }
+      if (organizationId) {
+        return await ctx.db.query("documents")
+        .withIndex("by_organization_id", (q) => q.eq("organizationId", organizationId))
+        .paginate(paginationOpts);
+      }
+        return await ctx.db.query("documents")
+        .withIndex("by_owner_id", (q) => q.eq("ownerId",user.subject))
+        .paginate(paginationOpts);
     },
 });
 
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,10 +6,12 @@ import { api } from "../../../convex/_generated/api";
 import { Navbar } from "./navbar"
 import { TemplatesGallery } from "./templates-gallery";
 import { DocumentsTable } from "./documents-table";
+import { useSearchParam } from "@/hooks/use-search-param";
 
 
 const Home = () => {
-  const { results, status, loadMore }  = usePaginatedQuery(api.documents.get, {}, { initialNumItems: 5 });
+  const[search] =useSearchParam();
+  const { results, status, loadMore }  = usePaginatedQuery(api.documents.get, {search}, { initialNumItems: 5 });
 
   return (
     <div className="min-h-screen flex flex-col">

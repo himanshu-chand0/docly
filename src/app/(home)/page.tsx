c61f2c206efba97e91cf4eb4b3b2080b6c64d325
Himanshu Chand <himanshu.chand@icloud.com>
2026-01-12 22:11:19 +0545

    commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,16 @@
-import Link from "next/link";
+"use client";
+
+import { usePaginatedQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
 
 import { Navbar } from "./navbar"
 import { TemplatesGallery } from "./templates-gallery";
+import { DocumentsTable } from "./documents-table";
+
 
 const Home = () => {
+  const { results, status, loadMore }  = usePaginatedQuery(api.documents.get, {}, { initialNumItems: 5 });
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="ficed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
@@ -11,6 +18,7 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplatesGallery />
+        <DocumentsTable documents = {results} loadMore = {loadMore} status = {status} />
       </div>
     </div>
   );

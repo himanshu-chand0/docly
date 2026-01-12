f05903c2dd3a96a77914d3a3fda1328c923db31d
Himanshu Chand <himanshu.chand@icloud.com>
2026-01-12 13:25:39 +0545

    commit

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -14,25 +14,30 @@ import {
   MenubarSubTrigger,
   MenubarTrigger,
 } from "@/components/ui/menubar";
+import { FileIcon } from "lucide-react";
 
 export const Navbar = () => {
   return (
     <nav className="flex items-center justify-between">
       <div className="flex gap-2 items-center">
         <Link href="/">
-          <Image src="/logo.svg" alt="logo" width={36} height={36} />
+          <Image src="/logo.svg" alt="logo" width={72} height={72} className="m-2" />
         </Link>
         <div className="flex flex-col">
-          <DocumentInput>
+          <DocumentInput />
             <div className="flex">
               <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
                 <MenubarMenu>
                   <MenubarTrigger>File</MenubarTrigger>
-                  {/* You can add MenubarContent here if needed */}
+                  <MenubarContent>
+                    <MenubarItem>
+                      <FileIcon />
+                      Save
+                    </MenubarItem>
+                  </MenubarContent>
                 </MenubarMenu>
               </Menubar>
             </div>
-          </DocumentInput>
         </div>
       </div>
     </nav>
